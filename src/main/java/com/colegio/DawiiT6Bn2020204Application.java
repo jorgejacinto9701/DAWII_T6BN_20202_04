ba26362b30f99f2513139852aab184371802b9b6
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2020-09-30 22:27:54 -0500

    Mi primer commit

---
+++ src/main/java/com/colegio/DawiiT6Bn2020204Application.java
@@ -0,0 +1,13 @@
+package com.colegio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiT6Bn2020204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiT6Bn2020204Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/colegio/DawiiT6Bn2020204ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiT6Bn2020204ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
