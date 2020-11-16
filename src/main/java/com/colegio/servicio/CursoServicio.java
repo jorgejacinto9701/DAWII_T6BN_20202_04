80fe5398419476f7c188e8c3c2899190a42725f9
Jhonny Sota <jhonnysota67@gmail.com>
2020-11-16 04:55:32 -0500

    implementacion de CrudCurso

--- src/main/java/com/colegio/servicio/CursoServicio.java
+++ src/main/java/com/colegio/servicio/CursoServicio.java
@@ -1,8 +1,17 @@
 package com.colegio.servicio;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.colegio.entidad.Curso;
 
 public interface CursoServicio {
 
 	public Curso insertaCurso(Curso obj);
+	public abstract void eliminarCurso(int id);
+	public abstract List<Curso>listarTodos();
+	public abstract List<Curso>listaPorNombre(String filtro);
+	public abstract Optional<Curso> buscarPorId(int id);
+	
+	
 }
--- src/main/java/com/colegio/servicio/CursoServicioImpl.java
+++ src/main/java/com/colegio/servicio/CursoServicioImpl.java
@@ -1,9 +1,13 @@
 package com.colegio.servicio;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.colegio.entidad.Curso;
+import com.colegio.entidad.Libro;
 import com.colegio.repositorio.CursoRepositorio;
 
 @Service
@@ -16,5 +20,24 @@ public class CursoServicioImpl implements CursoServicio {
 	public Curso insertaCurso(Curso obj) {
 		return cursoRepositorio.save(obj);
 	}
-
+	@Override
+	public void eliminarCurso(int id) {
+		 cursoRepositorio.deleteById(id);
+	}
+	@Override
+	public List<Curso> listarTodos() {
+		
+		return cursoRepositorio.findAll();
+	}
+	@Override
+	public List<Curso> listaPorNombre(String filtro) {
+		
+		return cursoRepositorio.listaPorNombre(filtro);
+	}
+	
+	@Override
+	public Optional<Curso> buscarPorId(int id) {
+		
+		return cursoRepositorio.findById(id);
+	}
 }
