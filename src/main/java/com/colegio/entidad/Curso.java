7f1ee719b116b5517000f5e6a4510ad6d4170c12
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2020-12-03 01:25:22 -0500

    cambios

--- src/main/java/com/colegio/entidad/Curso.java
+++ src/main/java/com/colegio/entidad/Curso.java
@@ -1,4 +1,5 @@
 package com.colegio.entidad;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,19 +10,20 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "curso")
 public class Curso {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(name = "idCurso")
-private Integer idCurso;
-private String codigo;
-private String nombre;
-private String estado;
-@ManyToOne
-@JoinColumn(name = "idSistemaEvaluacion")
-private SistemaEvaluacion sistemaevaluacion;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idCurso")
+	private Integer idCurso;
+	private String codigo;
+	private String nombre;
+	private String estado;
+	@ManyToOne
+	@JoinColumn(name = "idSistemaEvaluacion")
+	private SistemaEvaluacion sistemaevaluacion;
 }
